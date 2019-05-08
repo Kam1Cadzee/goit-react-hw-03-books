ab7a21ac567910eafc5cd5c8b755b536c21ff952
Kam1Cadzee <39504960+Kam1Cadzee@users.noreply.github.com>
2019-05-08 18:36:41 +0300

    Updates

---
+++ precache-manifest.889d5ab76ea46919486ffe243263d833.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e705aa89c3c69b0a8ecc217dce4f350b",
+    "url": "/goit-react-hw-03-books/index.html"
+  },
+  {
+    "revision": "50f4a1079b5c137743b6",
+    "url": "/goit-react-hw-03-books/static/css/main.1b0bbe7a.chunk.css"
+  },
+  {
+    "revision": "be2e4745e9e95182f042",
+    "url": "/goit-react-hw-03-books/static/js/2.3366088a.chunk.js"
+  },
+  {
+    "revision": "50f4a1079b5c137743b6",
+    "url": "/goit-react-hw-03-books/static/js/main.d6f04746.chunk.js"
+  },
+  {
+    "revision": "330c222c6e404f5d590b",
+    "url": "/goit-react-hw-03-books/static/js/runtime~main.1e7bb8d1.js"
+  }
+]);
